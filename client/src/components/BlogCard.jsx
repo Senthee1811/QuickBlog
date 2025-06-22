b7b31c9979abf8ce308f7a663f7f3759e54f60ad
Senthee1811 <esentheepan@gmail.com>
2025-06-22 21:52:15 +0530

    HomePage Doned

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -6,13 +6,15 @@ const BlogCard = ({blog}) => {
     const {title,description,category,image,_id} = blog; 
     const navigate = useNavigate();
   return (
-    <div onClick={() => navigate(`/blog/${_id}`)}>
+    <div onClick={() => navigate(`/blog/${_id}`)} 
+    className='w-full rounded-lg overflow-hidden shadow hover:scale-102 hover:shadow-primary/25
+    duration-300 cursor-pointer'>
         <img src={image} alt="" className='aspect-video'/>
         <span className='ml-5 mt-4 px-3 py-1 inline-block bg-primary/20 text-primary
         rounded-full  tetx-xs'>{category}</span>
         <div className='p-5'>
             <h5 className='mb-2 font-medium text-gray-900'>{title}</h5>
-            <p className='mb-3 text-xs text-gray-600'>{description.slice(0,80)}</p>
+            <p className='mb-3 text-xs text-gray-600' dangerouslySetInnerHTML={{"__html": description.slice(0,80)}}></p>
             </div>
         
     </div>
--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
-import { blogCategories } from '../assets/assets';
+import { blog_data, blogCategories } from '../assets/assets';
+// eslint-disable-next-line no-unused-vars
 import { motion } from "motion/react";
+import BlogCard from './BlogCard';
 
 
 const Bloglist = () => {
@@ -28,8 +30,10 @@ const Bloglist = () => {
             ) )}
 
         </div>
-        <div>
-            {/* Blog Cards */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4
+        gap-8 mb-24 mx-8 sm:x-16 xl:mx-40'>
+            {blog_data.filter((blog) => menu === "All" ? true : blog.category === menu ).
+            map((blog) => <BlogCard key={blog._id} blog={blog} />)}
         </div>
     </div>
   )
