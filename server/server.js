9c527f8173719316d757d6a65754985e18ac73d1
Senthee1811 <esentheepan@gmail.com>
2025-06-24 20:16:07 +0530

    Admin  Login Done

--- server/server.js
+++ server/server.js
@@ -2,17 +2,22 @@ import express from "express";
 import dotenv from "dotenv"; 
 import mongoose from "mongoose"; 
 import cors from "cors"; 
+import connectDB from "./configs/db.js";
+import adminRouter from "./routes/adminRoutes.js";
 
 
-const app = express(); 
+const app = express();  
+
+await connectDB();
 //Middlewares
 app.use(cors()); 
 app.use(express.json());  
-dotenv.config();
+dotenv.config(); 
 
 const port = process.env.PORT || 3001; 
 
-app.get("/", (req,res) => res.send("API working"))
+app.get("/", (req,res) => res.send("API working")); 
+app.use("/api/admin",adminRouter);
 
 
 app.listen(port,() => {
