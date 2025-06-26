7c73313531b53add4b81a713fa3c58dda0e82726
Senthee1811 <esentheepan@gmail.com>
2025-06-26 20:26:35 +0530

    fixed some typos and now its Fine

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"; 
-import { adminLogin, approveByCommentById, deleteComentById, getAllBlogsAdmin, getAllComments, getDashbaord } from "../controllers/adminController.js";
+import { adminLogin, approveByCommentById, deleteCommentById, getAllBlogsAdmin, getAllComments, getDashboard } from "../controllers/adminController.js";
 import auth from "../middleware/auth.js";
 
 
@@ -8,9 +8,9 @@ const adminRouter = express.Router();
 adminRouter.post("/login",adminLogin); 
 adminRouter.get('/comments',auth,getAllComments); 
 adminRouter.get('/blogs',auth,getAllBlogsAdmin); 
-adminRouter.post('/delete-comment',auth,deleteComentById); 
+adminRouter.post('/delete-comment',auth,deleteCommentById); 
 adminRouter.post('/approve-comment',auth,approveByCommentById); 
-adminRouter.get('/dashboard',auth,getDashbaord); 
+adminRouter.get('/dashboard',auth,getDashboard); 
 
 
 
