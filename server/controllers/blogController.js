dfdf009e3ed128a0839fdde27c97dcb2ab18e233
Senthee1811 <esentheepan@gmail.com>
2025-06-26 22:03:57 +0530

    AI added succesfully

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import imagekit from '../configs/imageKit.js';
 import Blog from '../models/Blog.js';
-import Comment from '../models/Comment.js';
+import Comment from '../models/Comment.js'; 
+import main from '../configs/gemini.js'
 
 export const addBlog = async(req,res)=> {
     try {
@@ -111,6 +112,17 @@ export const getBlogComments = async(req,res) => {
         }); 
         res.json({success:true,comments});
         
+    } catch (error) {
+        res.json({message:error.message,success:false});
+        
+    }
+} 
+
+export const generateContent = async(req,res) => {
+    try {
+        const {prompt} = req.body; 
+       const content =  await main(prompt + 'Generate a blog content for this topic in simple text format ');
+       res.json({success:true,content})
     } catch (error) {
         res.json({message:error.message,success:false});
         
